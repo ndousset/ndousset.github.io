905a35bb54c890652c7acc913efa0dfc35de597c
ndousset <ndousset@hotmail.com>
2017-10-05 12:15:43 +0200

    JS file
    JS file separated

---
+++ layout.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+  $(".navHeading, .selectedHeading").hover(function() {
+    $(this).addClass("bold");
+  }, function() {
+    $(this).removeClass("bold");
+    }
+  );
+
+  $("#homeLink").click(function(){
+    $("#devLink, #productLink, #contactLink, #bioLink").animate({"font-size":"22px"});
+    $("#homeLink").animate({"font-size":"46px"});
+    $("#home, #bio, #development, #product, #contact").hide(200);
+    $("#home").show(300);
+    $("body").css("background-image" , "url(images/pineapples-crop.jpg)");
+    }
+  );
+
+  $("#bioLink").click(function(){
+    $("#devLink, #productLink, #contactLink, #homeLink").animate({"font-size":"22px"});
+    $("#bioLink").animate({"font-size":"46px"});
+    $("#home, #bio, #development, #product, #contact").hide(200);
+    $("#bio").show(300);
+    $("body").css("background-image" , "url(https://s11.postimg.org/d35hanypf/DSC_0230.jpg");
+    }
+  );
+
+  $("#devLink").click(function(){
+    $("#homeLink, #productLink, #contactLink, #bioLink").animate({"font-size":"22px"});
+    $("#devLink").animate({"font-size":"46px"});
+    $("#home, #bio, #development, #product, #contact").hide(200);
+    $("#development").show(300);
+    $("body").css("background-image" , "url(images/steep.jpg)");
+    }
+  );
+
+  $("#productLink").click(function(){
+    $("#homeLink, #devLink, #contactLink, #bioLink").animate({"font-size":"22px"});
+    $("#productLink").animate({"font-size":"46px"});
+    $("#home, #bio, #development, #product, #contact").hide(200);
+    $("#product").show(300);
+    $("body").css("background-image" , "url(https://s4.postimg.org/czjgu2ov1/sky.jpg)");
+
+    }
+  );
+
+  $("#contactLink").click(function(){
+    $("#homeLink, #devLink, #productLink, #bioLink").animate({"font-size":"22px"});
+    $("#contactLink").animate({"font-size":"46px"});
+    $("#home, #bio, #development, #product, #contact").hide(200);
+    $("#contact").show(300);
+    $("body").css("background-image" , "url(images/outside.jpg)");
+    }
+  );
+});
